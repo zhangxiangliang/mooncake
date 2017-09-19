a266dbce890c7739056719d94b76ccfc6fdc79d4
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-19 23:46:06 +0800

    [测试用例] 创建基础测试用例、增加测试帮助函数

---
+++ tests/TestCase.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Tests;
+
+use PHPUnit\Framework\TestCase as TestCaseBase;
+
+class TestCase extends TestCaseBase
+{
+    /**
+     * 用户生成随机骰子数组
+     * @return array
+     */
+    public function generateDices()
+    {
+        return random_array(1, 6, 6);
+    }
+
+    /**
+     * 生成需要重复随机骰子
+     * @param  int $special 需要重复的数字
+     * @param  int $times  需要重复的次数
+     * @return array
+     */
+    public function generateRepeatDices($special, $times)
+    {
+        $count = 0;
+        $dices = [];
+        while($count < 6 - $times)
+        {
+            $rand = random_int(1, 6);
+            if($special === $rand) continue;
+
+            $count++;
+            array_push($dices, $rand);
+        }
+
+
+        foreach (range(1, $times) as $item) {
+            array_push($dices, $special);
+        }
+
+        return $dices;
+    }
+
+    /**
+     * 生成需要限制的随机骰子
+     * @param  int $special 需要限制的数字
+     * @param  int $times  需要限制的次数
+     * @return array
+     */
+    public function generateLimitDices($special, $times)
+    {
+        $count = 0;
+        $dices = [];
+
+        while($count < 6) {
+            $rand = random_int(1, 6);
+            if($rand === $special && $times == 1) continue;
+
+            $counts = array_count_values($dices);
+            $keys = array_keys($counts, $times-1);
+            if(in_array($special, $keys) && $rand === $special) continue;
+
+            $count++;
+            array_push($dices, $rand);
+        }
+        return $dices;
+    }
+}
