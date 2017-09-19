77c0fb030ceacb3bb92a22fd2c709a827a6be287
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-19 23:44:39 +0800

    [规则] 创建 一秀 规则、完成 一秀 测试用例

---
+++ src/Rules/YiXiuRule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Rules;
+
+class YiXiuRule extends RuleAbstract
+{
+    protected $name = '秀才';
+    protected $alias = '一秀';
+    protected $level = 1200;
+    protected $baseLevel = 1200;
+
+    public function rule(array $dices)
+    {
+        $count = array_count_values($dices);
+        $keys = array_keys($count, 1);
+
+        return in_array(4, $keys);
+    }
+}
---
+++ tests/Unit/YiXiuRuleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Tests\Unit;
+
+use Zhangxiangliang\Mooncake\Rules\YiXiuRule;
+use Zhangxiangliang\Mooncake\Tests\TestCase;
+
+class YiXiuRuleTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->rule = new YiXiuRule;
+    }
+
+    public function test_yi_xiu_rule_validate_true()
+    {
+        foreach (range(1, 10) as $r) {
+            $dices = $this->generateRepeatDices(4, 1);
+            $passed = $this->rule->validate($dices);
+            $this->assertEquals($passed, true);
+        }
+    }
+
+    public function test_yi_xiu_rule_validate_false()
+    {
+        foreach (range(1, 10) as $r) {
+            $dices = $this->generateLimitDices(4, 1);
+            $passed = $this->rule->validate($dices);
+            $this->assertEquals($passed, false);
+        }
+    }
+}
