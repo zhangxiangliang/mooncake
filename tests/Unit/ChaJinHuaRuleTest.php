ced32d244686fa9cf168b8545305cf89f77cd584
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-19 23:45:28 +0800

    [规则] 创建 插金花 规则 和 测试用例

---
+++ tests/Unit/ChaJinHuaRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Tests\Unit;
+
+use Zhangxiangliang\Mooncake\Rules\ChaJinHuaRule;
+use Zhangxiangliang\Mooncake\Tests\TestCase;
+
+class ChaJinHuaRuleTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->rule = new ChaJinHuaRule;
+    }
+
+    /**
+     * 验证 状元插金花
+     */
+    public function test_cha_jin_hua_validate_true()
+    {
+        $dices = [1, 1, 4, 4, 4, 4];
+        $passed = $this->rule->validate($dices);
+        $this->assertEquals($passed, true);
+    }
+
+    /**
+     * 验证除了 状元插金花 之外的规则
+     */
+    public function test_cha_jin_hua_validate_false()
+    {
+        foreach (range(1, 10) as $r) {
+            $dices = $this->generateDices();
+            if ($dices != [1, 1, 4, 4, 4, 4]) {
+                $passed = $this->rule->validate($dices);
+                $this->assertEquals($passed, false);
+            }
+        }
+    }
+}
