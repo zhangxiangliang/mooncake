1c6fa6f245cee05aca8627e62016cd32cfbb9512
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-21 21:51:51 +0800

    [规则] 创建 对堂 规则 并 完成测试用例

---
+++ src/Rules/DuiTangRule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Rules;
+
+class DuiTangRule extends RuleAbstract
+{
+    protected $dices = [1, 2, 3, 4, 5, 6];
+    protected $name = '榜眼';
+    protected $alias = '对堂';
+    protected $level = 500;
+}
---
+++ tests/Unit/DuiTangRuleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Tests\Unit;
+
+use Zhangxiangliang\Mooncake\Rules\DuiTangRule;
+use Zhangxiangliang\Mooncake\Tests\TestCase;
+
+class DuiTangRuleTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->rule = new DuiTangRule;
+    }
+
+    public function test_dui_tang_validate_true()
+    {
+        $dices = [1, 2, 3, 4, 5, 6];
+        $passed = $this->rule->validate($dices);
+        $this->assertEquals($passed, true);
+    }
+
+    public function test_dui_tang_validate_false()
+    {
+        $target = [1, 2, 3, 4, 5, 6];
+        foreach (range(1, 10) as $r) {
+            $dices = $this->generateDices();
+            sort($dices);
+            if ($dices !== $target) {
+                $passed = $this->rule->validate($dices);
+                $this->assertEquals($passed, false);
+            }
+        }
+    }
+}
