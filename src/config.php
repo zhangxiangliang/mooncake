d54ca5a6c7275851149b9fc213ce0cf57989952a
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-25 23:47:59 +0800

    [配置] 添加 规则 和 排序配置文件

---
+++ src/config.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+    'sorted' => 'desc',
+    'rules' => [
+        'chajinhua' => [
+            'name' => '状元',
+            'alias_name' => '状元插金花',
+            'field_name' => 'zhuangyuan',
+            'point' => 1200,
+            'level' => 1200,
+        ],
+        'mantanghong' => [
+            'name' => '',
+            'alias_name' => '满堂红',
+            'field_name' => 'mantanghong',
+            'point' => 1100,
+            'level' => 1100,
+        ],
+        'biandijin' => [
+            'name' => '状元',
+            'alias_name' => '遍地锦',
+            'field_name' => 'biandijin',
+            'point' => 1000,
+            'level' => 1000,
+        ],
+        'heiliubo' => [
+            'name' => '状元',
+            'alias_name' => '黑六勃',
+            'field_name' => 'heiliubo',
+            'point' => 900,
+            'level' => 900,
+        ],
+        'wuhong' => [
+            'name' => '状元',
+            'alias_name' => '五红',
+            'field_name' => 'wuhong',
+            'point' => 800,
+            'level' => 800,
+        ],
+        'wuzi' => [
+            'name' => '状元',
+            'alias_name' => '五子'
+            'field_name' => 'wuzi',
+            'point' => 700,
+            'level' => 700,
+        ],
+        'sihong' => [
+            'name' => '状元',
+            'alias_name' => '四红',
+            'field_name' => 'sihong',
+            'point' => 600,
+            'level' => 600,
+        ],
+        'duitang' => [
+            'name' => '榜眼',
+            'alias_name' => '对堂',
+            'field_name' => 'duitang',
+            'point' => 500,
+            'level' => 500,
+        ],
+        'tanhua' => [
+            'name' => '探花',
+            'alias_name' => '三红',
+            'field_name' => 'tanhua',
+            'point' => 400,
+            'level' => 400,
+        ],
+        'jinshi' => [
+            'name' => '进士',
+            'alias_name' => '四进',
+            'field_name' => 'jinshi',
+            'point' => 300,
+            'level' => 300,
+        ],
+        'juren' => [
+            'name' => '举人',
+            'alias_name' => '二举',
+            'field_name' => 'juren',
+            'point' => 200,
+            'level' => 200,
+        ],
+        'xiucai' => [
+            'name' => '秀才',
+            'alias_name' => '一秀',
+            'field_name' => 'xiucai',
+            'point' => 100,
+            'level' => 100,
+        ],
+    ]
+];
