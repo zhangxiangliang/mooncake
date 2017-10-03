84629ae6fb3ccda9ff3fff1230a793d23cb93319
ZHANGXIANGLIANG <326277403@qq.com>
2017-10-03 22:14:58 +0800

    [特性] 增加 规则配置 特性

---
+++ src/Traits/RuleConfigTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zhangxiangliang\Mooncake\Traits;
+
+trait RuleConfigTrait
+{
+    /**
+     * 设置 config 变量
+     */
+    private function setConfig($name)
+    {
+        $this->config = config('mooncake');
+    }
+
+    /**
+     * 获取默认配置名
+     */
+    private function getConfigDefault($name = '')
+    {
+        return $name
+            ? $this->config['default'][$name]
+            : $this->config['default'];
+    }
+
+    /**
+     * 获取默认规则
+     */
+    private function getConfigRules($name = '')
+    {
+        return $name
+            ? $this->config['rules'][$name]
+            : $this->config['rules'];
+    }
+}
