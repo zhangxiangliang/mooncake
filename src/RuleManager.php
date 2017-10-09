b6e66cddf6b58cb1f959dcf0dbc356bac8315791
ZHANGXIANGLIANG <326277403@qq.com>
2017-10-09 22:26:13 +0800

    [规则] 移动特性 到 规则管理类

--- src/RuleManager.php
+++ src/RuleManager.php
@@ -2,24 +2,17 @@
 
 namespace Zhangxiangliang\Mooncake;
 
-use Zhangxiangliang\Mooncake\Contracts\Rule;
-use Zhangxiangliang\Mooncake\Traits\RuleConfigTrait;
-use Zhangxiangliang\Mooncake\Traits\RuleIteratorTrait;
-use Zhangxiangliang\Mooncake\Traits\RuleValidateTrait;
+use Exception;
 
 class RuleManager
 {
     protected $rules;
     protected $config;
 
-    use RuleConfigTrait;
-    use RuleValidateTrait;
-    use RuleIteratorTrait;
-
     public function __construct(array $rules = [])
     {
-        $this->checkRulesInterface($rules);
-        $this->rules = $rules;
+        $this->validateRules($rules);
+        $this->setRules($rules);
         $this->setConfig(config('mooncake'));
     }
 
@@ -71,6 +64,9 @@ class RuleManager
      */
     public function getDices()
     {
+        if(!isset($this->dices)) {
+            throw new Exception('请设置塞子的值');
+        }
         return $this->ruleIterator('formatDices', $this->dices, [$this->dices]);
     }
 
@@ -78,9 +74,19 @@ class RuleManager
      * 获取 规则 相关信息
      * @return array
      */
-    public function getRuleConfig($name)
+    public function getRuleConfig($name = '')
     {
-        return config("mooncake.rules.{$name}");
+        if($name == '') {
+            return config("mooncake.rules");
+        }
+
+        $config = config("mooncake.rules.{$name}");
+
+        if(!$config) {
+            throw new Exception("请检查规则名称是否存在");
+        } else {
+            return $config;
+        }
     }
 
     /**
@@ -102,16 +108,137 @@ class RuleManager
     }
 
     /**
-     * 设置 规则数组
+     * 用于检查 规则是否符合要求
+     * @param  [type] $rules [description]
+     * @return [type]        [description]
      */
-    public function setRules($rules)
+    public function validateRules($rules)
     {
         $this->validateIsArray($rules);
         $this->validateRulesFieldName($rules);
         $this->checkRulesInterface($rules);
+    }
 
+    /**
+     * 用于检查 参数是否为数组 并进行错误抛出
+     */
+    public function validateIsArray($rules)
+    {
+        if(!is_array($rules)) {
+            throw new Exception("请检查参数是否为数组");
+        }
+    }
+
+    /**
+     * 用于检查 所有规则 是否存在于 Field 名中
+     */
+    public function validateRulesFieldName($rules)
+    {
+        $keys = collect($this->getConfigRules())->keys();
+        foreach ($rules as $ruleName => $rule) {
+            if(!$keys->contains($ruleName)) {
+                throw new Exception("请检查配置文件是否有该规则名");
+            };
+        }
+    }
+
+    /**
+     * 用于检查 所有规则 是否都实现了 Rule 接口
+     */
+    public function checkRulesInterface($rules)
+    {
+        foreach ($rules as $rule) {
+            if(!$this->checkRuleInterface($rule)) {
+                throw new Exception('请检查规则是否实现了 Rule 接口');
+            }
+        }
+    }
+
+    /**
+     * 用于检查 单个规则 是否实现了 Rule 接口
+     * @return false
+     */
+    public function checkRuleInterface($rule)
+    {
+        return $rule instanceof Rule;
+    }
+
+    /**
+     * 用于调用骰子方法
+     * @param  匹配到相应规则时，要调用的方法
+     * @param  没有匹配到相应规则时，返回默认值
+     * @param  调用的方法需要的参数
+     */
+    public function ruleIterator($method, $default, $arguments = null)
+    {
+        foreach ($this->rules as $rule) {
+            if ($rule->validate($this->dices)) {
+                return $arguments
+                    ? $rule->$method(...$arguments)
+                    : $rule->$method();
+            }
+        }
+        return $default;
+    }
+
+    /**
+     * 设置 规则数组
+     */
+    public function setRules($rules)
+    {
+        if(!isset($this->rules)) $this->rules = [];
         $this->rules = array_merge($this->rules, $rules);
-
         return $this;
+    }
+
+    /**
+     * 获取 config 变量
+     */
+    public function getConfig()
+    {
+        if(!isset($this->config)) {
+            throw new Exception("请检查 相关配置文件 是否存在");
+        }
+        return $this->config;
+    }
+
+    /**
+     * 设置 config 变量
+     */
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * 获取默认配置名
+     */
+    public function getConfigDefault($name = '')
+    {
+        if ($name == '') {
+            return $this->config['default'];
+        }
+
+        if (!isset($this->config['default'][$name])) {
+            throw new Exception("请检查 相关配置文件 的默认配置是否存在");
+        }
+
+        return $this->config['default'][$name];
+    }
+
+    /**
+     * 获取默认规则
+     */
+    public function getConfigRules($name = '')
+    {
+        if ($name == '') {
+            return $this->config['rules'];
+        }
+
+        if (!isset($this->config['rules'][$name])) {
+            throw new Exception("请检查 相关配置文件 的规则配置是否存在");
+        }
+
+        return $this->config['rules'][$name];
     }
 }
